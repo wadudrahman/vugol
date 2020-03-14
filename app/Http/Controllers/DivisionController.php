97f32564ca671f0cba22af147fedc07bf9ffcc5b
Wadud Rahman <mrahman143113@bscse.uiu.ac.bd>
2020-03-14 17:32:46 +0600

    Doctor Panel Correction 2

--- app/Http/Controllers/DivisionController.php
+++ app/Http/Controllers/DivisionController.php
@@ -2,20 +2,48 @@
 
 namespace App\Http\Controllers;
 
-use App\DivisionModel;
+use App\Models\DivisionModel;
 use Illuminate\Http\Request;
 
 class DivisionController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public static function addDivision(Request $request)
     {
-        //
+        $division = new DivisionModel();
+        $division->name = $request->name;
+        $division->name_bn = $request->name_bn;
+        $division->url = $request->url;
+        $division->save();
+
+        return response()->json($division);
     }
 
-    //
+    public static function divisionList()
+    {
+        return response()->json(DivisionModel::all());
+    }
+
+    public static function divisionDetails($id)
+    {
+        return response()->json(DivisionModel::find($id));
+    }
+
+    public static function updateDivisionDetails(Request $request, $id)
+    {
+        $division = DivisionModel::find($id);
+        $division->name = $request->input('name');
+        $division->price = $request->input('name_bn');
+        $division->description = $request->input('url');
+        $division->save();
+
+        return response()->json($division);
+    }
+
+    public function deleteDivision($id)
+    {
+        $division = DivisionModel::find($id);
+        $division->delete();
+
+        return response()->json('Division Deleted Successfully');
+    }
 }
